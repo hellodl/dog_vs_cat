c9e7bb6eefdb55201e5273b6fd11ef6b01e89cc3
helloDL <17820715@qq.com>
2017-09-16 23:23:21 +0800

    xx

--- dog_to_kaggle.py
+++ dog_to_kaggle.py
@@ -83,7 +83,7 @@ def ftLastFC():
                          'ReduceLROnPlateau':None,
                          'LearningRateScheduler':None,
                          'CSVLogger':None,
-                         'customize':['xk']
+                         'customize':[]
                      },
             },
             'time_format':'%m-%d_%X'
